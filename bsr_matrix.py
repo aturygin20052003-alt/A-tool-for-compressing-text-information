7ace487e11c8e75077c89578a3c1a80ff6e0e6b0
Matthew-Kotov <matveykotov2003@yandex.ru>
2026-05-05 20:50:59 +0300

    Update bsr_matrix.py

--- bsr_matrix.py
+++ bsr_matrix.py
@@ -1,11 +1,13 @@
 from scipy.sparse import bsr_matrix
+import numpy as np
 
-def to_bsr(sparse_matrix, b=1):
-    sparse = bsr_matrix(sparse_matrix, blocksize=(b,b))
-    return sparse
-
-matrix = [
-    [1,0],
-    [0,1]]
-res = to_bsr(matrix)
-print(res)
+def to_bsr(matrix: np.ndarray, b:int=1):
+    """
+    Преобразует плотную матрицу в блочно-разреженный формат
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.bsr_matrix.html
+    """
+    
+    if matrix.shape[0] % b != 0 or matrix.shape[1] % b != 0:
+        raise ValueError(f"Размеры матрицы {matrix.shape} должны нацело делиться на размер блока {b}")
+    sparse_matrix = bsr_matrix(matrix, blocksize=(b,b))
+    return sparse_matrix
