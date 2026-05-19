f2cd340df883ea176f585f05f6a1879ce58aa1b1
rudenick5 <arsenyi1233@gmail.com>
2026-05-19 13:04:25 +0300

    убран pickle

--- compression.py
+++ compression.py
@@ -1,21 +1,24 @@
-import gzip
 import lzma
+import gzip
 import struct
-import pickle
+import io
+import numpy as np
 
 def compress_matrix_gzip(matrix, b: int, output_filename: str):
     """
-    Склеивает заголовок 12 байт и матрицу, затем сжимает всё вместе через gzip.
+    Склеивает заголовок 12 байт и блочно-разреженную матрицу, затем сжимает всё вместе через gzip.
     """
     height, width = matrix.shape
 
     # 1. создание заголовка
     header = struct.pack('>iii', height, width, b)
 
-    # 2. Сериализаация матрицы
-    matrix_bytes = pickle.dumps(matrix)
+    # 2. Сериализация разреженной матрицы
+    buffer = io.BytesIO()
+    np.savez(buffer, data=matrix.data, indices=matrix.indices, indptr=matrix.indptr, blocksize=matrix.blocksize)
+    matrix_bytes = buffer.getvalue()
 
-    # 3. Соединеие и сжатие 
+    # 3. Соединение и сжатие
     full_data = header + matrix_bytes
     compressed_data = gzip.compress(full_data)
 
@@ -26,17 +29,19 @@ def compress_matrix_gzip(matrix, b: int, output_filename: str):
 
 def compress_matrix_lzma(matrix, b: int, output_filename: str):
     """
-    №Склеивает заголовок 12 байт и матрицу, затем сжимает всё вместе через lzma.
+    Склеивает заголовок 12 байт и блочно-разреженную матрицу, затем сжимает всё вместе через lzma.
     """
     height, width = matrix.shape
 
     # 1. создание заголовка
     header = struct.pack('>iii', height, width, b)
 
-    # 2. Сериализаация матрицы
-    matrix_bytes = pickle.dumps(matrix)
+    # 2. Сериализация разреженной матрицы
+    buffer = io.BytesIO()
+    np.savez(buffer, data=matrix.data, indices=matrix.indices, indptr=matrix.indptr, blocksize=matrix.blocksize)
+    matrix_bytes = buffer.getvalue()
 
-    # 3. Соединеие и сжатие 
+    # 3. Соединение и сжатие
     full_data = header + matrix_bytes
     compressed_data = lzma.compress(full_data)
 
